3e998c92a4ab76ad4b87e7108c6270f8b17ebf2c
Phạm Gia Huy <chickenhuy2005@gmail.com>
2025-10-09 22:36:01 +0700

    no message

--- BE/BE/src/main/java/swp391/fa25/swp391/repository/models/IncidentReportRepository.java
+++ BE/BE/src/main/java/swp391/fa25/swp391/repository/models/IncidentReportRepository.java
@@ -1,8 +1,10 @@
 package swp391.fa25.swp391.repository.models;
 
 import org.springframework.stereotype.Repository;
-import swp391.fa25.swp391.repository.GenericRepositoryImpl;
+
 import swp391.fa25.swp391.entity.IncidentReport;
+import swp391.fa25.swp391.repository.GenericRepositoryImpl;
+
 @Repository
 public class IncidentReportRepository extends GenericRepositoryImpl<IncidentReport> {
     public IncidentReportRepository() {
