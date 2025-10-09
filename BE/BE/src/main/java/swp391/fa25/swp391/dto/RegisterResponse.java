d11f598915d43451d47bb46e2cc32b55a5fa9ada
Phạm Gia Huy <chickenhuy2005@gmail.com>
2025-10-09 19:08:22 +0700

    Thêm các dto để xử lí request và response dễ dàng hơn

---
+++ BE/BE/src/main/java/swp391/fa25/swp391/dto/RegisterResponse.java
@@ -0,0 +1,15 @@
+package swp391.fa25.swp391.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegisterResponse {
+    private String message;
+    private Integer id;
+    private String username;
+    private String email;
+    private String role;
+    private String token;
+}
