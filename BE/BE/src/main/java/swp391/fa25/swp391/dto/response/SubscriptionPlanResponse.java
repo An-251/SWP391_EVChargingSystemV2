c69a4137ca2e17eaf7f66c8277993097bd3d57ed
An <annguyennhat11a221@gmail.com>
2025-10-16 22:45:46 +0700

    tao response cho subscriptionplan

---
+++ BE/BE/src/main/java/swp391/fa25/swp391/dto/response/SubscriptionPlanResponse.java
@@ -0,0 +1,20 @@
+package swp391.fa25.swp391.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+public class SubscriptionPlanResponse {
+
+    private Integer id;
+    private String planName;
+    private String planType;
+    private BigDecimal price;
+    private String validityDays;
+    private String description;
+
+    private int totalRegistrations; // Số lượng đăng ký liên quan
+}
