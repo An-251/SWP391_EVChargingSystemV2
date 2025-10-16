c86a2df8aa8f2e2a7d4867013a8f4b03819e8146
An <annguyennhat11a221@gmail.com>
2025-10-16 22:44:58 +0700

    Tao request cho facility

---
+++ BE/BE/src/main/java/swp391/fa25/swp391/dto/request/FacilityRequest.java
@@ -0,0 +1,35 @@
+package swp391.fa25.swp391.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size; // Import @Size
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class FacilityRequest {
+
+    @NotBlank(message = "Facility name cannot be blank")
+    @Size(max = 255, message = "Facility name cannot exceed 255 characters") // Giả sử cột NAME có độ dài mặc định là 255
+    private String name;
+
+    @NotBlank(message = "City cannot be blank")
+    @Size(max = 100, message = "City cannot exceed 100 characters")
+    private String city;
+
+    @NotBlank(message = "District cannot be blank")
+    @Size(max = 100, message = "District cannot exceed 100 characters")
+    private String district;
+
+    @NotBlank(message = "Ward cannot be blank")
+    @Size(max = 100, message = "Ward cannot exceed 100 characters")
+    private String ward;
+
+    @NotBlank(message = "Street address cannot be blank")
+    @Size(max = 255, message = "Street address cannot exceed 255 characters") // Giả sử cột STREET_ADDRESS có độ dài 255
+    private String streetAddress;
+
+    @NotNull(message = "Admin ID cannot be null")
+    private Integer adminId;
+}
