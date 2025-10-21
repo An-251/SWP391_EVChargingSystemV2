a89b8ba9993bac532e2d6e96d1c9dee3f946e9ec
Nguyễn Dương Ân <nguyenduongan64@gmail.com>
2025-10-21 14:01:08 +0700

    bỏ export admin cũ

--- FE/src/Page/AdminPage/index.js
+++ FE/src/Page/AdminPage/index.js
@@ -1,1 +1,8 @@
-export { default } from './AdminPage';
+export { default as Dashboard } from './Dashboard';
+export { default as FacilityList } from './Facilities';
+export { default as StationList } from './Stations';
+export { default as ChargingPointList } from './ChargingPoints';
+export { default as AccountList } from './Accounts';
+export { default as AdminRegistration } from './AdminRegistration';
+export { default as SubscriptionList } from './Subscriptions';
+export { default as Reports } from './Reports';
