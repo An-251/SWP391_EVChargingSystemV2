f09fd3cce65c27df5b58fb71923854c2eb99ecda
Nguyễn Dương Ân <nguyenduongan64@gmail.com>
2025-11-13 16:47:38 +0700

    export

---
+++ FE/src/Page/AdminPage/Invoices/index.js
@@ -0,0 +1,4 @@
+export { default as AdminInvoicesDashboard } from './AdminInvoicesDashboard';
+export { default as AdminInvoiceManagement } from './AdminInvoiceManagement';
+export { default as AllInvoicesView } from './AllInvoicesView';
+export { default as InvoiceList } from './InvoiceList'; // Keep old component for backward compatibility
